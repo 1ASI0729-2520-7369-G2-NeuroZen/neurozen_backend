144b6b641ecf343c713a9801b537a5e92a9101cd
Ax3LJnr <u20231g054@upc.edu.pe>
2025-11-01 20:09:30 -0500

    add : appointment-agreggates

---
+++ src/main/java/com/example/neurozen_platform/appointment/domain/model/aggregates/Professional.java
@@ -0,0 +1,13 @@
+package com.example.neurozen_platform.appointment.domain.model.aggregates;
+
+import com.example.neurozen_platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Professional extends AuditableAbstractAggregateRoot<Professional> {
+    
+
+    
+
+}
