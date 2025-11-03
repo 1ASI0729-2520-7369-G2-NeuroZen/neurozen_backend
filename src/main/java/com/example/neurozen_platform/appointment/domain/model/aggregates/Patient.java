bc98deed9d5cb34eebdc0ef5f68fee1230cab48b
Ax3LJnr <u20231g054@upc.edu.pe>
2025-11-02 20:55:59 -0500

    feat : patient aggregate methods

--- src/main/java/com/example/neurozen_platform/appointment/domain/model/aggregates/Patient.java
+++ src/main/java/com/example/neurozen_platform/appointment/domain/model/aggregates/Patient.java
@@ -1,10 +1,36 @@
 package com.example.neurozen_platform.appointment.domain.model.aggregates;
 
+
+import com.example.neurozen_platform.appointment.domain.model.valueobjects.ProfileId;
 import com.example.neurozen_platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
+
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Patient extends AuditableAbstractAggregateRoot<Patient> {
     
+    //Este sería una columna?
+    @Embedded
+    private ProfileId profileId;
+
+    public Patient(){
+
+    }
+
+    public Patient(Long profileId) {
+       this();
+       this.profileId = new ProfileId(profileId);
+    }
+
+    public Patient(ProfileId profileId){
+        this();
+        this.profileId = profileId;
+    }
+
+
+    public Long getProfileId(){
+        return this.profileId.profileId();
+    }
 }
