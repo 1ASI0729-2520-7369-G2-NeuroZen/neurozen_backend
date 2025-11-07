97ac6e77871cc606bf636c8d24f63e74d2bd86c7
Mig._.elJnr <88692906+Ax3LJnr@users.noreply.github.com>
2025-11-07 10:30:13 -0500

    add: expand MedicalAppointmentCommandService interface with command handling methods

--- src/main/java/com/example/neurozen_platform/appointment/domain/services/MedicalAppointmentCommandService.java
+++ src/main/java/com/example/neurozen_platform/appointment/domain/services/MedicalAppointmentCommandService.java
@@ -1,5 +1,24 @@
 package com.example.neurozen_platform.appointment.domain.services;
 
-public interface MedicalAppointmentCommandService {
 
+
+import com.example.neurozen_platform.appointment.domain.model.commands.CancelAppointmentCommand;
+import com.example.neurozen_platform.appointment.domain.model.commands.CompleteAppointmentCommand;
+import com.example.neurozen_platform.appointment.domain.model.commands.ConfirmAppointmentCommand;
+import com.example.neurozen_platform.appointment.domain.model.commands.RequestAppointmentCommand;
+
+
+public interface MedicalAppointmentCommandService {
+    
+    //Handle a request medical appointment commmand
+    Long handle(RequestAppointmentCommand command);
+
+    //Handle a cancel medical appointment command
+    Long handle(CancelAppointmentCommand command);
+
+    //Handle a complete medical appointment command
+    Long handle(CompleteAppointmentCommand command);
+
+    //Handle a confirm medical appointment command
+    Long handle(ConfirmAppointmentCommand command);
 }
